b21acaee23ebd28f9bab9668fc2eb878d4801e85
cbarone <cbarone@conexia.com>
2014-09-09 17:57:02 -0300

    Escena "Patito Feo"
    Se agregan huevos, patitos con esqueleto e intentos de scripts para la
    animación a través de botones.

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+
+	public Color defaultColour;
+	public Color selectedColour;
+	private Material mat;
+
+	void Start() {
+		mat = renderer.material;
+	}
+
+	void OnTouchDown() {
+		mat.color = selectedColour;
+	}
+
+	void OnTouchUp() {
+		mat.color = defaultColour;
+	}
+
+	void OnTouchStay() {
+		mat.color = selectedColour;
+	}
+
+	void OnTouchExit() {
+		mat.color = defaultColour;
+	}
+
+}
---
+++ Assets/Scripts/HideEgg.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideEgg : MonoBehaviour {
+
+	void Start() {
+	}
+
+	void OnTouchDown() {
+		//mat.color = selectedColour;
+		Animator.Play ("Blackout");
+	}
+
+	void OnTouchUp() {
+		//mat.color = defaultColour;
+	}
+
+	void OnTouchStay() {
+		//mat.color = selectedColour;
+	}
+
+	void OnTouchExit() {
+		//mat.color = defaultColour;
+	}
+
+}
