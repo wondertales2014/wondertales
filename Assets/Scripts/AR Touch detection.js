f18a41233e60cd24f86d83080f2538486e176f55
cbarone <cbarone@conexia.com>
2014-09-17 18:01:32 -0300

    Modificación escena Patito Feo
    Se agregan controles táctiles y juego de patito Feo.

---
+++ Assets/Scripts/AR Touch detection.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (Input.touchCount > 0)
+		{
+		for (var i = 0; i < Input.touchCount; ++i)
+			{
+			var ray = Camera.main.ScreenPointToRay (Input.GetTouch(i).position);
+			var hit:RaycastHit;		
+
+			if (Physics.Raycast (ray, hit,Mathf.Infinity)) 
+				{
+				if (Input.GetTouch(i).phase == TouchPhase.Began)
+					{
+					hit.transform.gameObject.SendMessage("OnTouchDown", 1);
+					}
+				if (Input.GetTouch(i).phase == TouchPhase.Ended)
+					{
+					hit.transform.gameObject.SendMessage("OnTouchDown", 2);
+					}
+				}
+			}
+		}
+}
---
+++ Assets/Scripts/AnimateEgg.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var anim : Animator;
+
+function Start () {
+
+	anim = GetComponent(Animator);
+}
+
+function Update () {
+
+	var fwd = transform.TransformDirection (Vector3.forward);
+	var hit:RaycastHit;
+	if (Physics.Raycast (transform.position, fwd, hit, Mathf.Infinity)) 
+		{
+		print("hit");
+		anim.SetBool("miraCentro", true);
+		}
+}
+
+function OnTouchDown (valor : int) {
+	
+	if (valor == 1)	
+		{
+		anim.SetBool("break", true);
+		}
+	if (valor == 2)	
+		{
+
+		}
+	if (valor == 3)
+		{
+		
+		}
+}
