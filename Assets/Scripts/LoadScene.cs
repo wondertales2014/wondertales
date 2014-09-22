ccca4f0f546f4803d1f09b6a4351b111f4043ce6
cbarone <chrisfyan@mb-chrisfyan.local>
2014-09-22 03:51:08 -0300

    Menu
    Se agrega un menu de inicio.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+	public void load(string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+
+	public void load(int sceneNumber) {
+		Application.LoadLevel (sceneNumber);
+	}
+}
