fe0e3c21f9b63c067d97eb6fc52c8ac08c1b7903
pa3lo <sig_pa3lo@yahoo.com>
2014-10-17 16:17:02 -0300

    Cambios en cuento 3 chanchitos
    Cambios en la primer escena.

---
+++ Assets/Scripts/DialogFlowControler.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var burbuja1 : Canvas;
+var burbuja2 : Canvas;
+var burbuja3 : Canvas;
+
+function Start () {
+
+}
+
+function Update () {
+
+
+burbuja1.enabled = true;
+
+var text = burbuja1.GetComponent(GUIText);
+text.text = "Vamonos chicos";
+}
