5234971d042c4db75303b6dbe507465593f5d14e
pa3lo <sig_pa3lo@yahoo.com>
2014-10-20 19:42:04 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Library/AssetServerCacheV3
    	Library/CurrentLayout.dwlt
    	Library/InspectorExpandedItems.asset
    	Library/ScriptAssemblies/Assembly-CSharp-Editor.dll
    	Library/ScriptAssemblies/Assembly-CSharp-Editor.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    	Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-CSharp.dll
    	Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Library/ScriptAssemblies/Assembly-UnityScript-Editor-firstpass.dll
    	Library/ScriptAssemblies/Assembly-UnityScript-Editor-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll
    	Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll.mdb
    	Library/ScriptAssemblies/Assembly-UnityScript.dll
    	Library/ScriptAssemblies/Assembly-UnityScript.dll.mdb
    	Library/ScriptMapper
    	Library/assetDatabase3
    	Library/expandedItems
    	Library/guidmapper
    	Library/metadata/e6/e6c92ebdaab659b4cb2fa7c3f4b1809f
    	Library/shadercompiler-1.log
    	Wondertales-csharp.sln
    	Wondertales.sln
    	Wondertales.userprefs
    
    Cambios cuento 3 chanchitos
    
    * Se finaliza pagina 3
    * Se añade maqueta
    de paginas 5 y 6.
    * Se mejora pagina 2.

---
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 3/Lobo.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var chozaAnim : Animator;
+var chanchoAnim : Animator;
+var loboAnim: Animator;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTouchDown (funcion : int) {
+
+if (funcion == 1)
+	{
+	loboAnim.SetBool("soplido",true);
+	chozaAnim.SetBool("volar", true);
+	chanchoAnim.SetBool("correr", true);
+	}
+}
