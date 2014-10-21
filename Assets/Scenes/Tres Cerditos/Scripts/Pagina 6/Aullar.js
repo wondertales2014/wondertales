d682cc5dff299103bc42b3d6696cc4cac6b47cf8
pa3lo <sig_pa3lo@yahoo.com>
2014-10-21 00:42:03 -0300

    Cambios en 3 chanchitos
    Se añaden animaciones y sonidos a la pagina 6

---
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 6/Aullar.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var loboAnim: Animator;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTouchDown (funcion : int) {
+
+if (funcion == 1)
+	{
+	loboAnim.SetBool("aullar",true);
+	audio.Play();
+	}
+}
