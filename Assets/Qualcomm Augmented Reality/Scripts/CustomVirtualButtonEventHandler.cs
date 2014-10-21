edb6991bfab9f6408e4b91782aa88cb253b2e2a6
cbarone <cbarone@conexia.com>
2014-10-21 18:22:03 -0300

    Se agregan botones virtuales
    Se agregan botones virtuales en patito feo

---
+++ Assets/Qualcomm Augmented Reality/Scripts/CustomVirtualButtonEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomVirtualButtonEventHandler : MonoBehaviour , 
+IVirtualButtonEventHandler {
+	
+	public GameObject smoke;
+	
+	void Start()
+	{
+		
+		VirtualButtonBehaviour[] vbs = GetComponentsInChildren<VirtualButtonBehaviour>();
+		for (int i = 0; i < vbs.Length; ++i)
+		{
+			vbs[i].RegisterEventHandler(this);
+		}
+
+	}
+	
+	
+	void Update () {
+		
+	}
+
+	public void OnButtonPressed( VirtualButtonAbstractBehaviour vb ){
+		
+		Debug.Log( vb.name +" Pressed" );
+		
+		smoke.SetActive (true);
+	}
+	
+	public void OnButtonReleased( VirtualButtonAbstractBehaviour vb ){
+		
+		Debug.Log( vb.name +" Released" );
+		
+	}
+}
