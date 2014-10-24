13c83d2836eb0297ef3fef98462ebd0b3e51a423
pa3lo <sig_pa3lo@yahoo.com>
2014-10-24 02:29:22 -0300

    Cambios 3 chanchitos y tutorial
    * Se añade animacion al lobo de la pagina 4.
    * Se arregla el tutorial (las dos primeras imagenes no salian)

---
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 5/Silla.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var chairAnim: Animator;
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTouchDown (funcion : int) {
+
+if (funcion == 1)
+	{
+	chairAnim.SetBool("mecer",true);
+	}
+if (funcion == 2)
+	{
+	chairAnim.SetBool("mecer",false);	
+	}
+}
