17122d74ae5bb7ee4c68a0353cdd69d01a6fa33a
Diegope <diegosperez87@gmail.com>
2014-11-16 19:41:07 -0200

    Caperucita Roja

---
+++ Assets/Scenes/Caperucita/scripts/Pagina3/FlorDesaparece.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var lobo : GameObject;
+var crack : AudioSource;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTouchDown (funcion : int) {
+
+if (funcion == 1)
+	{
+	lobo.SetActive(false);
+	crack.Play();
+	}
+if (funcion == 2)
+	{
+	lobo.SetActive(false);
+	crack.Play();
+	}
+if (funcion == 3)
+	{
+	lobo.SetActive(false);
+	crack.Play();
+	}
+if (funcion == 4)
+	{
+	lobo.SetActive(false);
+	crack.Play();
+	}
+}
