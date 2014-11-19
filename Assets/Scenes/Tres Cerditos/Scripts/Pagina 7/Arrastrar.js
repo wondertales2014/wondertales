364f475b61acf46dc34c82a83fe831b969e90c17
pa3lo <sig_pa3lo@yahoo.com>
2014-11-18 22:45:47 -0300

    Cambios escena 7 tres chanchitos

--- Assets/Scenes/Tres Cerditos/Scripts/Pagina 7/Arrastrar.js
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 7/Arrastrar.js
@@ -1,17 +1,25 @@
 ﻿#pragma strict
 
+private var camaraPos : Vector3;
+
 function Start () {
 
 }
 
 function Update () {
 
+for (var touch : Touch in Input.touches){
+        var ray = Camera.main.ScreenPointToRay(touch.position);
+        var hit : RaycastHit;
+        if (Physics.Raycast (ray, hit, Mathf.Infinity)) {
+            if(touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
+                var cameraTransform = Camera.main.transform.InverseTransformPoint(0, 0, 0);
+                transform.position = Camera.main.ScreenToWorldPoint(new Vector3 (touch.position.x, touch.position.y, cameraTransform.z));
+            }
+        }
+    }
 }
 
 function OnTouchDown (funcion : int) {
 
-if (funcion == 2)
-	{
-	
-	}
 }
