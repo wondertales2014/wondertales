bee97a4b183c6107c27d614ec60e478a36f320a3
pa3lo <sig_pa3lo@yahoo.com>
2014-11-30 23:25:34 -0300

    Cambios pagina 7 Tres Chanchitos
    Se añade sonido a la pagina y se hace desaparecer al lobo al llevarlo a
    la chimenea

--- Assets/Scenes/Tres Cerditos/Scripts/Pagina 7/Arrastrar.js
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 7/Arrastrar.js
@@ -1,9 +1,13 @@
 ﻿#pragma strict
 
-private var camaraPos : Vector3;
+var altura : UI.Text;
+var fondo : UI.Text;
+var Chimenea : GameObject;
+
+private var posicionInicial : Vector3;
 
 function Start () {
-
+posicionInicial = transform.position;
 }
 
 function Update () {
@@ -13,13 +17,31 @@ for (var touch : Touch in Input.touches){
         var hit : RaycastHit;
         if (Physics.Raycast (ray, hit, Mathf.Infinity)) {
             if(touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved) {
-            	if (transform.localPosition.y > 8.5)
+            	altura.text = "altura: " + transform.localPosition.y;
+            	fondo.text  = "fondo : " + transform.localPosition.x;
+            	if (transform.localPosition.y >= 0)
             		{
-            		transform.localPosition.z = 2.63; 
+            		transform.localPosition.z = 0;
+            		posicionInicial.z = Chimenea.transform.position.z;
             		}
                 var cameraTransform = Camera.main.transform.InverseTransformPoint(transform.position);
                 transform.position = Camera.main.ScreenToWorldPoint(new Vector3 (touch.position.x, touch.position.y, cameraTransform.z));
             }
+            if(touch.phase == TouchPhase.Ended) {
+				if ((transform.localPosition.y >= -24 && transform.localPosition.y <= 0) &&
+    				(transform.localPosition.x >= -24 && transform.localPosition.x <= 24))
+    				{
+    				var allChildRenderers = GetComponentsInChildren.<Renderer>();
+                    for ( var R in allChildRenderers)
+                    	{
+                    	R.enabled = false;
+                    	}           
+    				this.renderer.enabled = false;
+    				audio.Play();
+    				}
+
+            }
+
         }
     }
 }
---
+++ Assets/Scenes/Tres Cerditos/Scripts/Pagina 7/chimenea.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var altura : UI.Text;
+
+function Start () {
+
+}
+
+function Update () {
+
+altura.text = "altura: " + transform.position.y;
+}
