6870d515bc19ba684d751191d21165a64587fd48
Diegope <diegosperez87@gmail.com>
2014-12-02 02:18:35 -0200

    Caperucita Pagina 5 - Solo restan detalles

---
+++ Assets/Scenes/Caperucita/scripts/Pagina5/CaperucitaGame.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CaperucitaGame : MonoBehaviour {
+
+	public GameObject gato;
+	public GameObject pato;
+	public GameObject gallina;
+	public Text texto;
+	public AudioSource audioAnimal;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTouchDown(int valor) {
+		if (valor == 1)	
+		{
+			Collider[] gatoColliderComponents = gato.GetComponents<Collider>();
+
+			// Disable colliders:
+			foreach (Collider component in gatoColliderComponents)
+			{
+				component.enabled = false;
+			}
+
+			Collider[] gallinaColliderComponents = gallina.GetComponents<Collider>();
+			// Disable colliders:
+			foreach (Collider component in gallinaColliderComponents)
+			{
+				component.enabled = true;
+			}
+
+			audioAnimal.Play();
+			texto.text = "¿Quien regalo a Caperucita una caperusa color rojo?";
+		}
+		if (valor == 2)	
+		{
+
+		}
+		if (valor == 3)
+		{
+			
+		}
+	}
+
+}
---
+++ Assets/Scenes/Caperucita/scripts/Pagina5/WolfGame.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WolfGame : MonoBehaviour {
+	
+	public GameObject gato;
+	public GameObject pato;
+	public GameObject gallina;
+	public Text texto;
+	public AudioSource audioAnimal;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTouchDown(int valor) {
+		if (valor == 1)	
+		{
+			
+			Collider[] patoColliderComponents = pato.GetComponents<Collider>();
+			// Disable colliders:
+			foreach (Collider component in patoColliderComponents)
+			{
+				component.enabled = false;
+			}
+
+			audioAnimal.Play();
+			texto.text = "Felicitaciones! haz acertado todos los personajes!";
+		}
+		if (valor == 2)	
+		{
+			
+		}
+		if (valor == 3)
+		{
+			
+		}
+	}
+	
+}
