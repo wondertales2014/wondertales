c20095cb019994369b8ca6c86310035682e4694c
Diegope <diegosperez87@gmail.com>
2014-12-04 02:21:46 -0200

    Modificaciones

---
+++ Assets/Qualcomm Augmented Reality/Scripts/CRP6TrackableEventHandler.cs
@@ -0,0 +1,134 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Qualcomm Connected Experiences, Inc.
+==============================================================================*/
+
+using UnityEngine;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class CRP6TrackableEventHandler : MonoBehaviour,
+ITrackableEventHandler
+{
+	#region PRIVATE_MEMBER_VARIABLES
+	
+	private TrackableBehaviour mTrackableBehaviour;
+	
+	#endregion // PRIVATE_MEMBER_VARIABLES
+	
+	public Canvas screenController;
+
+	#region UNTIY_MONOBEHAVIOUR_METHODS
+	
+	void Start()
+	{
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+	
+	#endregion // UNTIY_MONOBEHAVIOUR_METHODS
+	
+	
+	
+	#region PUBLIC_METHODS
+	
+	/// <summary>
+	/// Implementation of the ITrackableEventHandler function called when the
+	/// tracking state changes.
+	/// </summary>
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+		    newStatus == TrackableBehaviour.Status.TRACKED ||
+		    newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			OnTrackingFound();
+		}
+		else
+		{
+			OnTrackingLost();
+		}
+	}
+	
+	#endregion // PUBLIC_METHODS
+	
+	
+	
+	#region PRIVATE_METHODS
+	
+	
+	private void OnTrackingFound()
+	{
+		this.screenController.enabled = false;
+		
+		Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		Terrain[]  terrainComponents  = GetComponentsInChildren<Terrain>(true);
+		//		Canvas[]   canvasComponents   = GetComponentsInChildren<Canvas>(true);
+		
+		// Enable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = true;
+		}
+		
+		// Enable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = true;
+		}
+		
+		// Enable terrain:
+		foreach (Terrain terrain in terrainComponents)
+		{
+			terrain.enabled = true;
+		}
+		Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+	}
+	
+	
+	private void OnTrackingLost()
+	{
+		this.screenController.enabled = true;
+
+		Renderer[] rendererComponents = GetComponentsInChildren<Renderer>(true);
+		Collider[] colliderComponents = GetComponentsInChildren<Collider>(true);
+		Terrain[]  terrainComponents  = GetComponentsInChildren<Terrain>(true);
+		Canvas[]   CanvasComponents   = GetComponentsInChildren<Canvas>(true);
+		
+		// Disable rendering:
+		foreach (Renderer component in rendererComponents)
+		{
+			component.enabled = false;
+		}
+		
+		// Disable colliders:
+		foreach (Collider component in colliderComponents)
+		{
+			component.enabled = false;
+		}
+		
+		// Disable terrain:
+		foreach (Terrain terrain in terrainComponents)
+		{
+			terrain.enabled = false;
+		}
+		
+		// Disable Canvas:
+		foreach (Canvas canvas in CanvasComponents)
+		{
+			canvas.enabled = false;
+		}
+		
+		Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+	}
+	
+	#endregion // PRIVATE_METHODS
+}
