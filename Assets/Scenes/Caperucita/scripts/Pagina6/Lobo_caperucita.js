8b9204e54e1372eebe3cad320bba809d3da23230
Diegope <diegosperez87@gmail.com>
2014-12-05 01:48:49 -0200

    Modificaciones pagina 6, pagina 2 y pagina 5

---
+++ Assets/Scenes/Caperucita/scripts/Pagina6/Lobo_caperucita.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var chanchoAnim : Animator;
+var loboAnim: Animator;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTouchDown (funcion : int) {
+
+if (funcion == 1)
+	{
+	loboAnim.SetBool("soplido",true);
+	chanchoAnim.SetBool("correr", true);
+	}
+}
